b7e7090707cdc03387f500e7114ea5a2c1e7eb12
jashua-d <denisjas@kean.edu>
2023-11-09 16:41:44 -0800

    updates

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,12 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return 
+}
+
+console.log(findUserByUsername(users, 'mlewis'))
+console.log(findUserByUsername(users, 'taco'))
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
